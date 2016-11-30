7060fd161457e96ed9dd4cfb4977eae6a668b991
Chris Wingo and Ryan Mathisen <chris+ryan@>
2016-11-30 10:41:48 -0800

    add a small amount of html, including result display and reset button, and appropriate routes

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,14 +11,34 @@ namespace CoinProgram
     {
       Get["/"] = _ =>
       {
+        Game.gameInstances.Clear();
         Game newGame = new Game("", "");
         return View["index.cshtml"];
       };
       Post["/choice1"] = _ =>
       {
         Game currentGame = Game.gameInstances[0];
-        string playerChoice = Request.Form["player1"];
-        System.Console.WriteLine(playerChoice);
+        currentGame.setChoice1(Request.Form["player1"]);
+        System.Console.WriteLine("Player 1 choice: " + currentGame.getChoice1());
+        return View["index.cshtml"];
+      };
+      Post["/choice2"] = _ =>
+      {
+        Game currentGame = Game.gameInstances[0];
+        currentGame.setChoice2(Request.Form["player2"]);
+        System.Console.WriteLine("Player 2 choice: " + currentGame.getChoice2());
+        return View["index.cshtml"];
+      };
+      Get["/outcome"] = _ =>
+      {
+        Game currentGame = Game.gameInstances[0];
+        string result = currentGame.DetermineWinner();
+        return View["index.cshtml", result];
+      };
+      Post["/reset"] = _ =>
+      {
+        Game.gameInstances.Clear();
+        Game newGame = new Game("", "");
         return View["index.cshtml"];
       };
     }
